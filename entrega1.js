869d2b4077db78bf65c24e87eb019dd240854b8b
ChristianCabrera22 <113479317+ChristianCabrera22@users.noreply.github.com>
2023-09-25 23:04:16 -0300

    Create entrega1.js

---
+++ entrega1.js
@@ -0,0 +1,60 @@
+class ProductManager {
+
+    constructor() {
+        this.Products = []; //base de datos productos (db)
+    }
+    addProduct(title, description, price, thumbnail, code, stock) {
+        if (!title || !description || !price || !thumbnail || !code || !stock) {
+            console.log("Error, all fields are required")
+        } else {
+        let product = this.Products.find(prod => prod.code === code) //verifica si el codigo esta en la db
+        if (product) {
+            return console.log("invalid code, existing")
+        } else {
+              this.Products.push(
+            {
+                id: this.Products.length + 1,
+                title,
+                description,
+                price,
+                thumbnail,
+                code,
+                stock
+            })
+            console.log("Product add")
+        }
+        }
+    }
+
+    getProdcuts() {
+        return this.Products
+    }
+
+    getProdcutById(id) {
+        let product = this.Products.find(prod => prod.id === id)
+        if (!product) {
+            console.log("Product not found")
+        }else {
+            console.log("product id:",id,product)
+        }
+    }
+}
+
+let productManager = new ProductManager();
+
+
+
+
+//Test:
+console.log("Products in db: ",productManager.getProdcuts());
+//formato: (title, description, price, thumbnail, code, stock)
+productManager.addProduct("prod1", "test1", 10, "img12.jpg", "prod2Test1", 11)
+productManager.addProduct("prod2", "test2", 20, "img13.jpg", "prod3Test1", 12)
+productManager.addProduct("prod3", "test3", 30, "img14.jpg", "prod4Test1", 13)
+productManager.addProduct("prod4", "test4", 40, "img15.jpg", "prod5Test1", 14)
+
+
+
+
+console.log("Products in db: ",productManager.getProdcuts());
+productManager.getProdcutById(2);
